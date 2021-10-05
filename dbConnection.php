0beec59ad5027fe8414a3e9de01123422fff160d
MohitBaldawa <mohitbaldawa@gmail.com>
2021-10-05 21:42:40 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,8 +1,15 @@
 <?php
-$db_host = "localhost:3307";
-$db_user = "root";
-$db_password = "";
-$db_name = "lms_db";
+// For Development connection in localhost (local machine)
+// $db_host = "localhost:3307";
+// $db_user = "root";
+// $db_password = "";
+// $db_name = "lms_db";
+
+// Remote Database Connection
+$db_host = "remotemysql.com";
+$db_user = "3zLkwr6ptq";
+$db_password = "OypWImdkpx";
+$db_name = "3zLkwr6ptq";
 
 // Create Connection
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
